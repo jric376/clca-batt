7e7388882ba831a846e35569a98c032c36260d2a
Julian Ricardo <Julian Ricardo>
2016-09-15 00:21:28 -0400

    Add initial plot attempt

--- ani_plots.R
+++ ani_plots.R
@@ -8,6 +8,7 @@ rm(list=ls())
 # setwd(as.character(wd_path))
 setwd("E:\\GitHub\\clca-batt")
 library("futile.logger")
+library("gganimate")
 library("ggplot2")
 src_list = list.files(pattern = "*load.R", full.names = TRUE)
 # "bldg_load", "grid_load", "pv_load"
@@ -16,17 +17,35 @@ source(src_list[1])
 test_bldg <- get_test_bldg()
 
 get_ani_bldg <- function(bldg) {
-  base_ts <- bldg$get_base_ts()
+  full_df <- bldg$get_base_ts()
+  full_df$run<- as.character(1)
+  full_df$week <- format(full_df$date_time, "%U")
   
-  for (i in seq.int(1,bldg$get_ts_count())) {
-    print(i)
+  for (i in seq.int(2,bldg$get_ts_count())) {
+    # kwh <- as.vector(bldg$get_ts_df(i))$kwh
+    rand_kwh <- as.data.frame(bldg$get_ts_df(i))
+    rand_kwh$run <- as.character(i)
+    rand_kwh$week <- format(rand_kwh$date_time, "%U")
+    full_df <- rbind(full_df, rand_kwh)
   }
-  ts_df1 <- as.data.frame(bldg$get_ts_df()[2])[2]
-  base_ts <- cbind(base_ts, ts_df1$kwh)
-  colnames(base_ts) <- c("date_time", "base_kwh", "rand1_kwh")
-  base_ts$month <- format(base_ts$date_time, "%m")
-  jul_ts <- subset(base_ts, base_ts$month == "07")[1:2016,]
-  return(plot(jul_ts$date_time, jul_ts$base_kwh, type = "l"))
-  # points(jul_ts$date_time, jul_ts$rand1_kwh, col = "gray55")
+  
+  jul_df <- subset(full_df, full_df$week == "29") # mid-July, when elec demand peaks
+  jul_plt <- ggplot(data = jul_df, aes(date_time, kwh)) +
+                    geom_point() +
+                    geom_line(data = subset(jul_df, jul_df$run == 1),
+                                mapping = aes(frame = run),
+                                colour = "white", size = 1) +
+                    geom_point(data = subset(jul_df, jul_df$run == 1),
+                                mapping = aes(frame = run),
+                                colour = "#00CC66", size = 1.5) +
+                    labs(x = "",
+                         y = "kWh",
+                         title = "Medium Office Elec. (July)") +
+                    theme(panel.background = element_rect(colour = "gray75", fill = "gray80")) +
+                    theme(panel.grid.major = element_line(colour = "gray85")) +
+                    theme(panel.grid.minor = element_line(colour = "gray85"))
+  ani_jul_plt <- gg_animate(jul_plt)
+  
+  return(ani_jul_plt)
 }
-
+get_ani_bldg(test_bldg)
