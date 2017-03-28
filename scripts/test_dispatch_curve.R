de08fe5fd522d89753260f9d2445f24d0ca8ce98
Julian Ricardo <jric376@users.noreply.github.com>
2017-03-28 09:37:11 -0400

    Add get_test_curve, add unit tests based on "NYISO" dispatch curve

---
+++ scripts/test_dispatch_curve.R
@@ -0,0 +1,47 @@
+library(testthat)
+
+setwd("..")
+source("scripts/dispatch_curve.R")
+
+test_that("Test disp_curv: cost stats columns", {
+            disp <- get_test_curv("nyiso")  
+  
+            expected <- c("plprmfl", "MC_mean", "MC_sd")
+            actual <- disp$get_mc_stats()
+            expect_named(actual, expected)
+})
+
+test_that("Test disp_curv: ISO plants length", {
+            disp <- get_test_curv("nyiso")
+  
+            expected <- 327
+            actual <- nrow(disp$get_iso_plants())
+            expect_equal(actual, expected)
+})
+
+test_that("Test disp_curv: cost join w/o NAs", {
+            disp <- get_test_curv("nyiso")
+            
+            costs <- anyNA(select(disp$get_iso_plants(),
+                                  starts_with("MC")))
+            expect_false(costs)
+})
+
+test_that("Test disp_curv: emissions calculated w/o NAs", {
+            disp <- get_test_curv("nyiso")
+            
+            emish <- anyNA(select(disp$get_dispatch(),
+                                  ends_with("plc2erta")))
+            expect_false(emish)
+})
+
+test_that("Test disp_curv: color labels", {
+            disp <- get_test_curv("nyiso")
+            
+            expected <- 0
+            empty_colors <- nrow(filter(disp$get_dispatch(),
+                                          fuel_type == "black")
+            )
+            expect_equal(empty_colors, expected)
+            
+})
